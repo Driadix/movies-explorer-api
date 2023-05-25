ea62e980f046aa03e8fe64d7097dcb44929b982f
Driadix <59824288+Driadix@users.noreply.github.com>
2023-05-25 10:53:46 +0300

    feat: basic project features
    init, base settings/configs, create models, base middlewares, utils

---
+++ models/movie.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const URL_REGEX = require('../utils/constants');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: [true, 'Необходимо передать поле country в запрос'],
+  },
+  director: {
+    type: String,
+    required: [true, 'Необходимо передать поле director в запрос'],
+  },
+  duration: {
+    type: Number,
+    required: [true, 'Необходимо передать поле duration в запрос'],
+  },
+  year: {
+    type: String,
+    required: [true, 'Необходимо передать поле year в запрос'],
+  },
+  description : {
+    type: String,
+    required: [true, 'Необходимо передать поле description  в запрос'],
+  },
+  image: {
+    type: String,
+    required: [true, 'Необходимо передать поле image в запрос'],
+  },
+  trailerLink: {
+    type: String,
+    required: [true, 'Необходимо передать поле trailerLink в запрос'],
+    validate: URL_REGEX,
+  },
+  thumbnail : {
+    type: String,
+    required: [true, 'Необходимо передать поле thumbnail в запрос'],
+    validate: URL_REGEX,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: [true, 'Необходимо передать поле owner в запрос'],
+  },
+  movieId: {
+    type: Number,
+    required: [true, 'Необходимо передать поле owner в запрос'],
+  },
+  nameRU: {
+    type: String,
+    required: [true, 'Необходимо передать поле year в запрос'],
+  },
+  nameEN: {
+    type: String,
+    required: [true, 'Необходимо передать поле year в запрос'],
+  },
+},
+{ timestamps: true, versionKey: false },
+);
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Diploma movie api
+** write prj description
